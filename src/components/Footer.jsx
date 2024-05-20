6247a7e81b84a2e1fcbcb62bf6f062eb72676295
Cristian Gomez <cristian.gomez.r1089@gmail.com>
2024-05-20 12:43:12 -0300

    Modificando footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,58 +5,68 @@ import logo from '../assets/logo.png';
 import { FaInstagram } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa6";
 
-const Footer = () => {
-  return (
-    <footer className="py-6 bg-white text-[#F27B35]">
-      <div className="container px-6 mx-auto space-y-6 divide-y divide-gray-600 divide-opacity-50">
-        <div className="grid grid-cols-12">
-          <div className="col-span-full md:col-span-6">
-            <a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 md:justify-start">
-              <div className="flex items-center justify-center w-24 h-24 rounded-full">
-                <img src={logo} alt="" />
-              </div>
-              <span className="self-center text-2xl font-semibold">Fundacion Lemuel</span>
-            </a>
-          </div>
-          <div className="col-span-6 text-center md:text-left md:col-span-3">
-            <p className="pb-1 text-lg font-medium">Navegación</p>
-            <ul>
-              {navLinks.map((nav) => (
-                <li key={nav.id}>
-                  <NavLink
-                    to={`/${nav.id}`}
-                    className="text-[18px] font-medium  hover:text-orange-700"
-                  >
-                    {nav.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="col-span-6 text-center md:text-left md:col-span-3">
-            <p className="pb-1 text-lg font-medium">Dirección</p>
-            <ul>
-              <li>Caleta Olivia - Santa Cruz</li>
-            </ul>
-          </div>
-        </div>
-        <div className="grid justify-center pt-6 lg:justify-between">
-          <div className="flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6">
-            <span>© 2024 Fundacion Lemuel. All rights reserved</span>
-          </div>
-          <div className="flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13">
-            <a rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=100081438307111&locale=az_AZ" target='_blanck' title="Facebook" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-600 dark:text-gray-50">
-              <FaFacebook className='w-7 h-7'/>
-            </a>
 
-            <a rel="noopener noreferrer" href="https://www.instagram.com/fundacion_lemuel_co/" target='_blanck' title="Instagram" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-violet-600 dark:text-gray-50">
-              <FaInstagram className='w-7 h-7' />
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+const Footer = () => {
+    return (
+    
+        <footer class="bg-white ">
+            <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div class="md:flex md:justify-between">
+                    <div class="mb-6 md:mb-0">
+                        <a href="https://flowbite.com/" class="flex items-center">
+                            <img src={logo} class="h-8 me-3" alt="FlowBite Logo" />
+                        </a>
+                    </div>
+                    <div class="grid grid-cols-2 gap-8 sm:gap-6">
+                        <div>
+                            <h2 class="mb-6 text-sm font-semibold text-orange-700 uppercase ">Nosotros</h2>
+                            <ul class="text-orange-500  font-medium">
+                                {navLinks.map((nav) => (
+                                    <li class="mb-2" key={nav.id}>
+                                        <NavLink
+                                            to={`/${nav.id}`}
+                                            lass="hover:underline"
+                                        >
+                                            {nav.title}
+                                        </NavLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        
+                        <div>
+                            <h2 class="mb-6 text-sm font-semibold text-orange-700 uppercase ">Legal</h2>
+                            <ul class="text-orange-500  font-medium">
+                                <li class="mb-2">
+                                    <a href="#" class="hover:underline">Politicas de Privacidad</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="hover:underline">Terminos y Condiciones</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-6 border-orange-500 sm:mx-auto  lg:my-8" />
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <span class="text-sm text-orange-600 sm:text-center ">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+                    </span>
+                    <div class="flex mt-4 sm:justify-center sm:mt-0">
+                        <a href="https://www.facebook.com/profile.php?id=100081438307111&locale=az_AZ" target='_blank' class="text-orange-500 hover:text-gray-900 ">
+                            <FaFacebook size={20} />
+                            <span class="sr-only">Facebook page</span>
+                        </a>
+                        <a href="https://www.instagram.com/fundacion_lemuel_co/" target='_blank' class="text-orange-500 hover:text-gray-900  ms-5">
+                            <FaInstagram size={20} />
+                            <span class="sr-only">Discord community</span>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+    );
 };
 
 export default Footer;
